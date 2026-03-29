667462c4f401e8fcf09d86ae54022f7d5c61cbb8
KevinZRZ85 <kevin@example.com>
2026-03-29 09:19:05 +0800

    Build for production

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  // Cloudflare Pages 支持
-  output: "standalone",
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone',
   images: {
     unoptimized: true,
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
